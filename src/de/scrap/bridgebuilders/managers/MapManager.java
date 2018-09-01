b858d613cc687c1a02cf36d0d9656719072495b7
Scrapnix <battlemac182@googlemail.com>
2018-09-01 16:41:26 +0200

    Updates

--- src/de/scrap/bridgebuilders/managers/MapManager.java
+++ src/de/scrap/bridgebuilders/managers/MapManager.java
@@ -1,9 +1,25 @@
 package de.scrap.bridgebuilders.managers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.MaxChangedBlocksException;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldedit.data.DataException;
+import com.sk89q.worldedit.schematic.MCEditSchematicFormat;
+import com.sk89q.worldedit.schematic.SchematicFormat;
+
+import de.orochimaruu.bridgebuilders.Main;
 
 public class MapManager {
 
@@ -14,6 +30,26 @@ public class MapManager {
 	
 	public static void startScheduler() {
 		
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void loadAndSetSchematic(String name, Location loc) {
+		File schematic = new File("plugin/BridgeBuilders/schematics" + name);
+		Main.getWorldEdit().getWorldEdit();
+		EditSession session = Main.getWorldEdit().getWorldEdit().getEditSessionFactory().getEditSession(new BukkitWorld(LocationManager.get("orientationpoint").getWorld()), WorldEdit.getInstance().getConfiguration().maxChangeLimit);
+		SchematicFormat format = SchematicFormat.getFormat(schematic);
+		try {
+			CuboidClipboard clipboard = format.load(schematic);
+			Vector v = new Vector(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+			clipboard.paste(session, v, true);
+		} catch (DataException | IOException e) {
+			Bukkit.getConsoleSender().sendMessage(Main.prefix + "§cDie Schematic §a" + name + " §cwurde nicht gefunden!");
+			e.printStackTrace();
+		} catch (MaxChangedBlocksException e) {
+			Bukkit.getConsoleSender().sendMessage(Main.prefix + "§cZu viele Blöcke wurden versucht zu ändern!");
+			e.printStackTrace();
+		}
+
 	}
 	
 }
--- src/de/scrap/bridgebuilders/schedulers/MapManagingScheduler.java
+++ src/de/scrap/bridgebuilders/schedulers/MapManagingScheduler.java
@@ -1,8 +1,12 @@
 package de.scrap.bridgebuilders.schedulers;
 
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import de.orochimaruu.bridgebuilders.Main;
+import de.orochimaruu.bridgebuilders.listener.PlayerJoinListener;
+import de.scrap.bridgebuilders.managers.LocationManager;
 import de.scrap.bridgebuilders.managers.MapManager;
 
 public class MapManagingScheduler implements Runnable{
@@ -19,27 +23,115 @@ public class MapManagingScheduler implements Runnable{
 
 	@Override
 	public void run() {
-		
-		if (!MapManager.waitinglist.isEmpty()) {
-			Player p = MapManager.waitinglist.get(0);
-			if (!MapManager.createdSections.isEmpty()) {
-				
-				try {
-					MapManager.waitinglist.remove(p);
-					for (int i = 0; i < MapManager.createdSections.size(); i++) {
-						int size = MapManager.size.get(p);
-						if (MapManager.createdSections.get(size + i) == true) {
-							//Breite der Section = 20Blöcke
-//							Location loc = new Location(world, x, y, z) Erstmal setup command erstellen (Kannst du das bitte machen, habe schon nen LocationManager erstellt, ist nicht so schwierig ;))
+		if (Main.setup == true) {
+			if (!MapManager.waitinglist.isEmpty()) {
+				Player p = MapManager.waitinglist.get(0);
+				Player p2 = null;
+				if (!MapManager.createdSections.isEmpty()) {
+					try {
+						MapManager.waitinglist.remove(p);
+						MapManager.playing.add(p);
+						if (PlayerJoinListener.team.containsKey(p)) {
+							MapManager.playing.add(PlayerJoinListener.team.get(p));
+							p2 = PlayerJoinListener.team.get(p);
 						}
+						for (int i = 0; i <= MapManager.createdSections.size(); i++) {
+							int size = MapManager.size.get(p);
+							if (MapManager.createdSections.get(size + i) == true) {
+								//Breite der Section = 21Blöcke (20 Spielfeld, 1 Trennblock) [Orientationpoint hat auch Barriere]
+								Location loc1 = LocationManager.get("orientationpoint");
+								Location loc2 = new Location(loc1.getWorld(), loc1.getBlockX(), loc1.getBlockY(), loc1.getBlockZ() + (i * 21));
+								int soloduo = Integer.valueOf(String.valueOf(size).substring(0, 0));
+								int blocks = Integer.valueOf(String.valueOf(size).substring(1, 2));
+								
+								if (soloduo == 1) {
+									Location loc3 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 10.5);
+									if (blocks == 20) { 
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 30) { 
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 40) {
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 50) {
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 60) {
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 70) {
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 80) {
+										p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}
+								}else if (soloduo == 2) {
+									Location loc3 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 5.5);
+									Location loc4 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 16.5);
+									if (blocks == 20) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 30) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 40) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 50) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 60) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 70) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 80) {
+										p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F); p2.playSound(p2.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}
+								}
+							}else if (i == MapManager.createdSections.size()) {
+								int i2 = MapManager.createdSections.size() + 1;
+								Location loc1 = LocationManager.get("orientationpoint");
+								Location loc2 = new Location(loc1.getWorld(), loc1.getBlockX(), loc1.getBlockY(), loc1.getBlockZ() + (i2 * 21));
+								int soloduo = Integer.valueOf(String.valueOf(size).substring(0, 0));
+								int blocks = Integer.valueOf(String.valueOf(size).substring(1, 2));
+
+								if (soloduo == 1) {
+									Location loc3 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 10.5);
+									if (blocks == 20) {
+										MapManager.loadAndSetSchematic("bridgebuilders120.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 30) {
+										MapManager.loadAndSetSchematic("bridgebuilders130.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 40) {
+										MapManager.loadAndSetSchematic("bridgebuilders140.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 50) {
+										MapManager.loadAndSetSchematic("bridgebuilders150.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 60) {
+										MapManager.loadAndSetSchematic("bridgebuilders160.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 70) {
+										MapManager.loadAndSetSchematic("bridgebuilders170.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 80) {
+										MapManager.loadAndSetSchematic("bridgebuilders180.schematic", loc2); p.teleport(loc3); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}   
+								}else if (soloduo == 2) {
+									Location loc3 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 5.5);
+									Location loc4 = new Location(loc2.getWorld(), loc2.getX() + 3.5, loc2.getY() + 1, loc2.getZ() + 16.5);
+									if (blocks == 20) {
+										MapManager.loadAndSetSchematic("bridgebuilders220.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 30) {
+										MapManager.loadAndSetSchematic("bridgebuilders230.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 40) {
+										MapManager.loadAndSetSchematic("bridgebuilders240.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 50) {
+										MapManager.loadAndSetSchematic("bridgebuilders250.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 60) {
+										MapManager.loadAndSetSchematic("bridgebuilders260.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 70) {
+										MapManager.loadAndSetSchematic("bridgebuilders270.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									}else if (blocks == 80) {
+										MapManager.loadAndSetSchematic("bridgebuilders280.schematic", loc2); p.teleport(loc3); p2.teleport(loc4); p.playSound(p.getLocation(), Sound.FIREWORK_LAUNCH, 1F, 1F);
+									} 
+								}
+							}
+						}
+					} catch (Exception e) {
+						// TODO: handle exception
 					}
-				} catch (Exception e) {
-					// TODO: handle exception
+				}else {
+					
 				}
-				
 			}
 		}
-		
 	}
-
 }
